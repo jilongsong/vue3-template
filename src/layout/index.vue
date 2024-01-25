fba305d84fbe535b40e1dc8c56b2fb7d2b5f7d2e
songjilong <songjilong@threebody.xyz>
2024-01-25 22:39:52 +0800

    build: 更新配置文件

---
+++ src/layout/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const homeref = ref('xxxxxxx')
+</script>
+<template>
+	<div>
+		<el-button>{{ homeref }}</el-button>
+	</div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>homeref</div>
+</template>
+
+<style scoped lang="scss"></style>

15363e24074c9e8d20eaebe12dd5e45cda4ad079
songjilong <songjilong@threebody.xyz>
2024-01-26 19:25:34 +0800

    update: test

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div style="width: 100%; height: 100vh; margin: auto">
-    <h1>{{ homeref }}</h1>
-    <div><el-button type="primary" size="large" @click="homeref = 'hello! my baby'">点击这里</el-button></div>
+    <h1>{{ homered }}</h1>
+    <div><el-button type="primary" size="large" @click="homered = 'hello! my baby'">点击这里</el-button></div>
   </div>
 </template>
 
 <script setup lang="ts">
-const homeref = ref('');
+const homered = ref('');
 </script>
 
 <style scoped lang="scss"></style>
